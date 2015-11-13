6d362dca52fe804089cf779bea0a177232e84a3b
Chew Chee Ming <cheeming@infinite-code.com>
2015-11-13 17:55:30 +0800

    Add script to loop thru hostnames and dump data to file

---
+++ ping-to-gdocs/ping_all_and_save.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import ping
+import json
+
+
+def run():
+    hostnames = [
+        '8.8.8.8',  # google dns 1
+        'cn.pool.ntp.org',  # china ntp server
+    ]
+    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    results = ping.ping_servers(hostnames)
+    with open('{}.json'.format(timestamp), 'w') as fd:
+        json.dump({
+            'timestamp': timestamp,
+            'results': results,
+        }, fd)
+
+
+if __name__ == '__main__':
+    run()
